4754f842d3ced9e3de9a33e2d718efba594ddc6b
Pradnya <Pradnya@DESKTOP-Q2UFRV2>
2022-03-02 14:55:43 +0530

    First commit

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi from local");
+		System.out.println("hi from local...");
 
 	}
 
