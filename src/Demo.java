7c4008b70dbb458a22229993cbb0076244cb3d6e
Pradnya777777 <100761969+Pradnya777777@users.noreply.github.com>
2022-03-02 15:05:34 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi from local...");
+		System.out.println("hi from local..Pradnya.");
+		
 
 	}
 
